17e1dd67cf98ade11b84bd1251e066bbfa65a0dd
SAURABH <saurabhgoel1480@gmail.com>
2023-09-24 12:10:40 +0530

    final UI

--- tta-react-project/src/App.jsx
+++ tta-react-project/src/App.jsx
@@ -4,23 +4,23 @@ function App() {
 
   return (
     <>
-      <div className='website'>
-          <div className='navbar'>
-            <div className='lorem'>
+      <div className='m-24 flex flex-col'>
+          <div className='flex'>
+            <div className='border-2 border-solid border-black w-4/12 p-5'>
               <b>LOREM IPSUM</b>
             </div>
-            <div className='link1'>
+            <div className='border-2 border-solid border-black w-8/12 p-5 text-right border-l-0'>
               <b>LINK 1</b>
             </div>
           </div>
-          <div className='hero_section'>
-            <div className='hero_content'>
-              <b className='hero_main_content'>THIS IS SOME CONTENT ALIGNED IN THE CENTER VERTICALLY AND HORIZONTALLY</b>
+          <div className='flex border-2 border-solid border-black border-t-0 h-64'>
+            <div className='border-r-2 border-solid border-black w-6/12 flex text-center justify-center items-center p-12'>
+              <b>THIS IS SOME CONTENT ALIGNED IN THE CENTER VERTICALLY AND HORIZONTALLY</b>
             </div>
-            <div className='hero_empty'>
+            <div className='w-6/12'>
             </div>
           </div>
-          <div className='footer'>
+          <div className='border-2 border-solid border-black flex items-center text-left h-24 border-t-0 p-10'>
             <b>THIS IS THE FOOTER CONTENT WHICH IS ALIGNED LEFT</b>
           </div>
       </div>
