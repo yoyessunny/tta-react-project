68a9aacb1383dbfd37f31d30f0fda64d6914eb63
SAURABH <saurabhgoel1480@gmail.com>
2023-09-24 11:06:37 +0530

    UI created

--- tta-react-project/src/App.jsx
+++ tta-react-project/src/App.jsx
@@ -5,12 +5,24 @@ function App() {
   return (
     <>
       <div className='website'>
-        <div className='lorem'>
-          <b>Lorem Ipsum</b>
-        </div>
-        <div className='link1'>
-          <b>LINK 1</b>
-        </div>
+          <div className='navbar'>
+            <div className='lorem'>
+              <b>LOREM IPSUM</b>
+            </div>
+            <div className='link1'>
+              <b>LINK 1</b>
+            </div>
+          </div>
+          <div className='hero_section'>
+            <div className='hero_content'>
+              <b className='hero_main_content'>THIS IS SOME CONTENT ALIGNED IN THE CENTER VERTICALLY AND HORIZONTALLY</b>
+            </div>
+            <div className='hero_empty'>
+            </div>
+          </div>
+          <div className='footer'>
+            <b>THIS IS THE FOOTER CONTENT WHICH IS ALIGNED LEFT</b>
+          </div>
       </div>
     </>
   )
